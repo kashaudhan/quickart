f55baa1b6eb142f4cf406548be2b13194796e13d
Dharmendra Kashaudhan <kashaudhan94@gmail.com>
2024-03-24 13:56:36 +0530

    chore: accounts forms & views

---
+++ accounts/forms.py
@@ -0,0 +1,58 @@
+from django import forms
+from django.forms import fields
+from django.forms.forms import Form
+from .models import Account, UserProfile
+
+class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter password',
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm password',
+    }))
+    class Meta:
+        model = Account
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter first name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter last name'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter email address'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter phone number'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+    def clean(self):
+        cleaned_date = super(RegistrationForm, self).clean()
+        password = cleaned_date.get('password')
+        confirm_password = cleaned_date.get('confirm_password')
+
+        if password != confirm_password:
+            raise forms.ValidationError(
+                'Password does not match'
+            )
+
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = Account
+        fields = ('first_name', 'last_name', 'phone_number')
+
+    def __init__(self, *args, **kwargs):
+        super(UserForm, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+    
+
+
+class UserProfileFrom(forms.ModelForm):
+    profile_picture = forms.ImageField(required=False, error_messages = {'invalid': ('Image files only')}, widget=forms.FileInput)
+    class Meta:
+        model = UserProfile
+        fields = ('address_line_1', 'address_line_2', 'city', 'state', 'country', 'profile_picture')
+
+    def __init__(self, *args, **kwargs):
+        super(UserProfileFrom, self).__init__(*args, **kwargs)
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
